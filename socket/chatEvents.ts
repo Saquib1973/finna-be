3f6545148d49def79d2af25470c43cf9fe37bebc
Saquib Ali <saquibali353@gmail.com>
2025-10-14 17:53:21 +0530

    feat: chat completion

--- socket/chatEvents.ts
+++ socket/chatEvents.ts
@@ -1,7 +1,40 @@
 import { Socket, Server as SocketIOServer } from 'socket.io'
 import Conversation from '../modals/Conversation.js'
+import Message from '../modals/Message.js'
 
 export function registerChatEvents(io: SocketIOServer, socket: Socket) {
+
+  socket.on("getConversations", async () => {
+    try {
+      
+      const userId = socket.data.userId
+      if (!userId) {
+        socket.emit("getConversations", { success: false, msg: 'User not authenticated' })  
+        return
+      }
+
+      const conversations = await Conversation.find({
+        participants: userId,
+      })
+      .sort({ updatedAt: -1 })
+        .populate({
+          path: 'participants',
+          select: 'name username email avatar',
+        }).
+        populate({
+          path: 'lastMessage',
+          populate: {
+            path: 'senderId',
+            select: 'name avatar',
+          }
+        })
+        .lean()
+      socket.emit("getConversations", { success: true, data: conversations })
+    } catch (error) {
+    console.error('Error fetching conversations:', error)
+    socket.emit("getConversations", { success: false, msg: 'Failed to fetch conversations' })  
+    }
+  })
   socket.on('newConversation', async (data) => {
     console.log('New conversation event received:', data)
 
@@ -57,19 +90,76 @@ export function registerChatEvents(io: SocketIOServer, socket: Socket) {
       }
 
       // Emit to the room for all participants
-      io.emit(conversation._id.toString(), 'newConversation', {
+      io.to(conversation._id.toString()).emit('newConversation', {
         success: true,
         data: { ...populatedConversation, isNew: true },
       })
 
       // Also emit directly to the requesting socket for immediate response
-      socket.emit('newConversation', {
-        success: true,
-        data: { ...populatedConversation, isNew: true },
-      })
+      // socket.emit('newConversation', {
+      //   success: true,
+      //   data: { ...populatedConversation, isNew: true },
+      // })
     } catch (error) {
       console.error('Error handling new conversation:', error)
       socket.emit('error', { msg: 'Failed to create conversation' })
+    }
+  })
+
+  socket.on("newMessage", async (data) => {
+    console.log('New message event received:', data)
+    try {
+      const message = await Message.create({
+        conversationId:data.conversationId,
+        senderId:data.sender.id,
+        content:data.content,
+        attachment:data.attachment,
+      })
+      io.to(data.conversationId).emit("newMessage", { success: true, data: {
+        id:message._id,
+        content:message.content,
+        sender:{
+          id:data.sender.id,
+          name:data.sender.name,
+          avatar:data.sender.avatar,
+        },
+        attachment:message.attachment,
+        createdAt:new Date().toISOString(),
+        conversationId:data.conversationId,
+      } })
+      await Conversation.findByIdAndUpdate(data.conversationId, {lastMessage:message._id})
+
+    } catch (error) {
+      console.error('Error handling new message:', error)
+      socket.emit("newMessage", { success: false, msg: 'Failed to send message' })
+    }
+  })
+  socket.on("getMessages", async (data:{conversationId:string}) => {
+    console.log('New message event received:', data)
+    try {
+      const messages = await Message.find({conversationId:data.conversationId})
+      .sort({ createdAt: -1 })
+      .limit(50)
+      .populate<{senderId:{_id:string,name:string,avatar:string}}>({
+        path: 'senderId',
+        select: 'name avatar',
+      }).lean()
+      const messageWithSender = messages.map(msg=>({
+        id:msg._id,
+        content:msg.content,
+        sender:{
+          id:msg.senderId?._id,
+          name:msg.senderId.name,
+          avatar:msg.senderId.avatar,
+        },
+        attachment:msg.attachment,
+        createdAt:msg.createdAt,
+        conversationId:msg.conversationId,
+      }))
+      socket.emit("getMessages", { success: true, data: messageWithSender })
+    } catch (error) {
+      console.error('Error handling new message:', error)
+      socket.emit("getMessages", { success: false, msg: 'Failed to send message' })
     }
   })
 }
